4d4b52f71a689ee5fc7b8f7e988402ebaaaf97d1
leydifranco40 <158230623+leydifranco40@users.noreply.github.com>
2024-09-25 11:09:58 -0500

    Ejercicio nro 6

--- empleado.ts
+++ empleado.ts
@@ -12,6 +12,10 @@ trabajar(horas:number){
     console.log(`El empleado ${this.nombre} trabaja ${horas} horas al día` )
 }
 
+saludar(): void {
+    console.log(`Hola! ${this.nombre} tu edad es ${this.getEdad()} y el salario es de: ${this.salario} `)
+}
 }
 const Empleado1 = new Empleado ("Andres", 30, 2000)
+Empleado1.saludar()
 Empleado1.trabajar(8)
